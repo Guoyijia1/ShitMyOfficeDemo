77b6fb680b191679c562322943a37ef96d101aca
Yijia Guo <guoy032@newschool.edu>
2023-12-01 19:25:53 -0500

    ,

---
+++ ShitMyOfficeDemo/Assets/Karting/Scripts/KartSystems/AddForceOnKeyPress.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+
+public class AddForceOnKeyPress : MonoBehaviour
+{
+
+    public float forceMagnitude = 10f; // Adjust the force strength as needed
+    public KeyCode keyToPress = KeyCode.M;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Ensure there is a Rigidbody component attached to the GameObject
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody component not found on the GameObject.");
+            enabled = false; // Disable the script
+        }
+    }
+
+    private void Update()
+    {
+        // Check if the specified key is pressed
+        if (Input.GetKeyDown(keyToPress))
+        {
+            // Apply force in the forward direction of the GameObject
+            rb.AddForce(transform.forward * forceMagnitude, ForceMode.Impulse);
+        }
+    }
+}
---
+++ ShitMyOfficeDemo/Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput1 : BaseInput
+    {
+        public string TurnInputName = "Hotizontal";
+        public string AccelerateButtonName = "Accelerate";
+        public string BrakeButtonName = "Brake";
+
+        public override InputData GenerateInput() {
+            return new InputData
+            {
+                Accelerate = Input.GetButton(AccelerateButtonName),
+                Brake = Input.GetButton(BrakeButtonName),
+                TurnInput = Input.GetAxis("Horizontal")
+            };
+        }
+    }
+}
